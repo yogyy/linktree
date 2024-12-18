d54388a36baba98ae4f70f35049c079238b44113
yogyy <yogimr261@gmail.com>
2024-12-19 01:27:13 +0700

    feat: add vue

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import Links from "./components/Links.vue";
+</script>
+
+<template>
+  <Links />
+</template>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.Vue";
+
+createApp(App).mount("#app");
